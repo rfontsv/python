fc562143e5d4f20845bd954fdca4f782e90216cc
fontsvr <133181178+fontsvr@users.noreply.github.com>
2024-07-21 20:42:33 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,7 +9,22 @@ import urllib3
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
         return'''
-def inicia()
+        
+from os.path import dirname, abspath, join
+dir = dirname(abspath(__file__))
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        with open(join(dir, '..', 'api', '01.Aficionado.w3u'), 'r') as file:
+          for line in file:
+            self.wfile.write(line.encode())
+        return  
+        
+def inicia():
    target_url="https://raw.githubusercontent.com/rfontsv/python/main/api/01.Aficionado.w3u"
    http = urllib3.PoolManager()
    response = http.request('GET', target_url)
