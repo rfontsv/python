a64e317e376a54f0d37b789c0dbc286b5bcee57e
fontsvr <133181178+fontsvr@users.noreply.github.com>
2024-07-21 20:20:24 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,18 @@
 from http.server import BaseHTTPRequestHandler
 import urllib3
 
-class handler(BaseHTTPRequestHandler):
+'''class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        '''self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))'''
-        target_url="https://raw.githubusercontent.com/rfontsv/python/main/api/01.Aficionado.w3u"
-        http = urllib3.PoolManager()
-        response = http.request('GET', target_url)
-        data = response.data.decode('utf-8')
-        return
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return'''
+def inicia()
+   target_url="https://raw.githubusercontent.com/rfontsv/python/main/api/01.Aficionado.w3u"
+   http = urllib3.PoolManager()
+   response = http.request('GET', target_url)
+   data = response.data.decode('utf-8')
+   
+inicia()
